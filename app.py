45ab1c9ab30232fdab52f9a6b2a80d4cee994a6b
Michal Szafranek <michal.szafranek@gmail.com>
2026-05-05 14:53:10 +0200

    commit 1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -18,4 +18,36 @@ stores = [
 def get_stores():
     return {"stores": stores}
 
-    #test commit
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201 #default status code, whe have to returnn something
+
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data['name'], "price": request_data['price']}
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "Storage not nound"}, 404
+
+
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"message": "Store not found"}, 404
+
+
+@app.get("/store/<string:name>/item")
+def get_items_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}
+    return {"message": "Store not found"}, 404
